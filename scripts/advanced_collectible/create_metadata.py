e16c65f7ad40cdee0f38dcad92d87c8a0d600f94
Lam <lam.trinh@global.t-bird.edu>
2022-09-01 02:34:14 +0900

    NFT created on Opensea testnet, only thing left is to write test for uploading to ipfs

--- scripts/advanced_collectible/create_metadata.py
+++ scripts/advanced_collectible/create_metadata.py
@@ -41,8 +41,7 @@ def write_metadata(token_ids, nft_contract):
         )
         if Path(metadata_file_name).exists():
             print(
-                "{} already found, delete it to overwrite!".format(
-                    metadata_file_name)
+                "{} already found, delete it to overwrite!".format(metadata_file_name)
             )
         else:
             print("Creating Metadata file: " + metadata_file_name)
@@ -54,8 +53,7 @@ def write_metadata(token_ids, nft_contract):
             )
             image_to_upload = None
             if os.getenv("UPLOAD_IPFS") == "true":
-                image_path = "./img/{}.png".format(
-                    breed.lower().replace('_', '-'))
+                image_path = "./img/{}.png".format(breed.lower().replace("_", "-"))
                 image_to_upload = upload_to_ipfs(image_path)
             image_to_upload = (
                 breed_to_image_uri[breed] if not image_to_upload else image_to_upload
@@ -66,22 +64,21 @@ def write_metadata(token_ids, nft_contract):
             if os.getenv("UPLOAD_IPFS") == "true":
                 upload_to_ipfs(metadata_file_name)
 
+
 # curl -X POST -F file=@metadata/rinkeby/0-SHIBA_INU.json http://localhost:5001/api/v0/add
 
 
 def upload_to_ipfs(filepath):
-    with Path(filepath).open("rb") as fp:
+    with Path(filepath).open("rb") as fp:  # rb is reading in as binary
         image_binary = fp.read()
+        # Upload to IPFS
         ipfs_url = (
-            os.getenv("IPFS_URL")
-            if os.getenv("IPFS_URL")
-            else "http://localhost:5001"
+            os.getenv("IPFS_URL") if os.getenv("IPFS_URL") else "http://localhost:5001"
         )
-        response = requests.post(ipfs_url + "/api/v0/add",
-                                 files={"file": image_binary})
+        response = requests.post(ipfs_url + "/api/v0/add", files={"file": image_binary})
         ipfs_hash = response.json()["Hash"]
+        # "./img/0-PUG.png" --> "0-PUG.png"
         filename = filepath.split("/")[-1:][0]
-        image_uri = "ipfs://{}?filename={}".format(
-            ipfs_hash, filename)
+        image_uri = "ipfs://{}?filename={}".format(ipfs_hash, filename)
         print(image_uri)
     return image_uri
